118eb963f6036d230bd9435bdf45f2a4194902b9
Fatemeh Mokhtari <ftm.mkti.developermail@gmail.com>
2025-09-24 01:27:40 +0330

    add swiper

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Navbar from "@/modules/Navbar";
+import Slider from "@/template/Slider";
 
 
 
@@ -7,6 +8,7 @@ export default function Home() {
 
       <>
           <Navbar />
+          <Slider/>
       </>
   );
 }
